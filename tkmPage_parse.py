2d3f2f2cb5003455266f9a2911213a1b22bd0fda
kovacsja <janos.kovacs.h@gmail.com>
2021-09-07 22:11:05 +0200

    initial version

---
+++ tkmPage_parse.py
@@ -0,0 +1,59 @@
+from bs4 import BeautifulSoup
+from selenium import webdriver
+import pandas as pd
+import numpy as np
+
+url: str = (
+    "https://tkm.mnb.hu/?it=1&sp=&fpt=1&page=1&pagesize=100&orderCol=5&desc=False"
+)
+browser = webdriver.Chrome("/home/honfi/Documents/Python/chromedriver")
+tkm_html = browser.get(url)
+browser.implicitly_wait(10)
+institutes = browser.find_elements_by_class_name("institute-name")
+
+for i in institutes:
+    i.click()
+    browser.implicitly_wait(10)
+
+soup = BeautifulSoup(browser.page_source, "lxml")
+
+browser.close()
+rows = soup.find_all("tr", {"class": ["prow", "subtabletr"]})
+
+df_input = []
+for r in rows:
+    res = []
+    if r.find("div", class_="first-column"):
+        inst = r.find("div", class_="institute-name").text.strip()
+        prod = r.find("td", class_="td-notfirst").text.strip()
+    elif "%" in r.text:
+        res.append(inst)
+        res.append(prod)
+        for d in r.find_all("td"):
+            res.append(d.text.strip())
+    if len(res) > 0:
+        df_input.append(res)
+
+df = pd.DataFrame(df_input)
+df.columns = ["Intézmény", "Termék", "Alap", "Y5", "Y10", "Y15", "Y20", "Y30"]
+df["Y5"] = df["Y5"].str.replace("%", "")
+df["Y10"] = df["Y10"].str.replace("%", "")
+df["Y15"] = df["Y15"].str.replace("%", "")
+df["Y20"] = df["Y20"].str.replace("%", "")
+df["Y30"] = df["Y30"].str.replace("%", "")
+
+df["Y5"] = df["Y5"].str.replace(",", ".")
+df["Y10"] = df["Y10"].str.replace(",", ".")
+df["Y15"] = df["Y15"].str.replace(",", ".")
+df["Y20"] = df["Y20"].str.replace(",", ".")
+df["Y30"] = df["Y30"].str.replace(",", ".")
+
+df = df.replace("", np.NaN)
+
+df["Y5"] = df["Y5"].astype(float)
+df["Y10"] = df["Y10"].astype(float)
+df["Y15"] = df["Y15"].astype(float)
+df["Y20"] = df["Y20"].astype(float)
+df["Y30"] = df["Y30"].astype(float)
+
+df.to_excel("tkm.xlsx")
